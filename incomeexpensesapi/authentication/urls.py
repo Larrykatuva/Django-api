cabfbf33f359fca54eb5dcf8b456006643494fcd
Larrykatuva <larry.katuva@gmail.com>
2021-03-07 10:24:28 +0300

    Resfresh token setup

--- incomeexpensesapi/authentication/urls.py
+++ incomeexpensesapi/authentication/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('email-verify/', views.VerifyEmail.as_view(), name='email-verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
